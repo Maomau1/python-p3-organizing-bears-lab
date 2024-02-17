1c824a98d6c555f14217e5af9fe010c7b2bc230a
maurice diong <mdiong00@gmail.com>
2024-02-17 16:42:23 -0500

    completes test

---
+++ lib/insert.sql
@@ -0,0 +1,9 @@
+INSERT INTO bears (name,sex) VALUES
+('Mr. Chocolate','M'),
+('Rowdy','M'),
+('Tabitha','M'),
+('Sergeant Brown','M'),
+('Melissa','F'),
+('Grinch','F'),
+('Wendy','F'),
+(Null,'F');
